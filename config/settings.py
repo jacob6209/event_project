5804f8b224d3b6dadbb12674ba208acaaf75aa62
یعقوب ابراهیمی <4709964807@mipogc.org>
2025-12-02 08:38:25 +0330

    add static files with css,font,...

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# static file config
+STATIC_URL = "/static/"
+STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
